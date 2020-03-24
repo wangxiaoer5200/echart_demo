2cbf85870af8ae8234bb59f05189c325e6dd52f4
王斌 <18695686631@163.com>
2020-03-24 16:22:56 +0800

    fix: 新增例子目录；

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view />
+    <p>
+      <router-link to='/'>首页</router-link>
+      <ul>
+        <li>
+          <router-link to='/test'>例子1</router-link>
+        </li>
+        <li>
+          <router-link to='/test2'>例子2</router-link>
+        </li>
+        <li>
+          <router-link to='/test2'>例子3</router-link>
+        </li>
+        <li>
+          例子4
+        </li>
+        <li>
+          例子5
+        </li>
+      </ul>
+    </p>
+    <router-view style="display:flex;justify-content: center;;" />
   </div>
 </template>
 
@@ -18,6 +37,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
